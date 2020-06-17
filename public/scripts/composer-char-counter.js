4b92404e030a7956561ab580e3acc68331a31072
adun1 <bmccormack15@gmail.com>
2020-06-17 23:04:48 +0000

    Implemented the character count with the logic inside the composer-char-counter.js file. Added to new-tweet.css is the class to change the colour of the counter text. Index.html has been changed to include this new javascript file

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {
+  // this callback runs once the document is "ready"
+  const MAX_TXT_LEN = 140; 
+  
+  // used keyup as we want to get the length of the textbox aftet the charcter added
+  $('.new-tweet form textarea').keyup(function() {
+    // console.log(MAX_TXT_LEN - this.textLength);
+    //traverse up dowm tree and down to child text
+    const lenRem = MAX_TXT_LEN - $(this).val().length;
+    
+    let counterObj = $(this).parent().children().find('.counter');
+
+    if(lenRem < 0) {
+      counterObj.addClass("negative")
+    } else {
+      counterObj.removeClass("negative");
+    }
+    counterObj.text(lenRem);    
+  })
+});
