1ce564541027ec05d8f16ce5321f6d7f44b14f67
adun1 <bmccormack15@gmail.com>
2020-06-18 22:15:56 +0000

    cleaned up client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,22 +4,8 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// this callback runs once the document is "ready"
 $(document).ready(() => {
-  // this callback runs once the document is "ready"
-  /*
-  const data = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  };
-  */
-
  const data = [
     {
       "user": {
@@ -47,8 +33,8 @@ $(document).ready(() => {
   
   const renderTweets = function(tweets) {
     for(const tweet of tweets) {
-      const $item = createTweetElement(tweet);
-      $('#tweet-container').append($item);
+      const $tweetHTML = createTweetElement(tweet);
+      $('#tweet-container').append($tweetHTML);
     }
   }
 
@@ -77,10 +63,5 @@ $(document).ready(() => {
     </article>`);
   }
   renderTweets(data);
-
-  // console.log(createTweetElement(data));
-  // console.log(createTweetElement(data));
-  // console.log(createTweetElement(data));
-  // const $tweet = $(`<article class="tweet">Hello world</article>`);
  
 });
