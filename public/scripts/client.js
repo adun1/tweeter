2f67dbe2641281ee8be54038d426563333bcff08
adun1 <bmccormack15@gmail.com>
2020-06-19 02:53:23 +0000

    implemented load tweets both on startup and after posting a tweet. Removed first tweet in html as that is now handled by server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,50 +6,29 @@
 
 // this callback runs once the document is "ready"
 $(document).ready(() => {
- const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   $('.new-tweet form').on('submit', function(event) {
     event.preventDefault();
     //use this.serialize to get all the values at once
     const data = $(this).serialize();
     $.post('/tweets', data)
-    .then(function(data1) {
-      //debugging (if successful do something)
-      console.log("data1: ", data1);
+      .then(function(data1) {
+      loadTweets();
     });
-
-  ;
   });
 
-
+  //hard coded get method location for now (can change later)
+  const loadTweets = function() {
+    $.getJSON('/tweets')
+      .then(function(data) {
+      $('#tweet-container').empty();
+      renderTweets(data);
+    });
+  };
   
   const renderTweets = function(tweets) {
     for(const tweet of tweets) {
       const $tweetHTML = createTweetElement(tweet);
-      $('#tweet-container').append($tweetHTML);
+      $('#tweet-container').prepend($tweetHTML);
     }
   }
 
@@ -77,6 +56,6 @@ $(document).ready(() => {
       </footer>
     </article>`);
   }
-  renderTweets(data);
+  loadTweets();
  
 });
